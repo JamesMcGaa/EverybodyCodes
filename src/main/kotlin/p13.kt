ad988037a64ca44b8cfc5a21032858fedfa70f2e
James McGaa <jemcgaa@gmail.com>
2025-02-05 21:11:06 -0500

    p13

--- src/main/kotlin/p13.kt
+++ src/main/kotlin/p13.kt
@@ -1,16 +1,26 @@
 import kotlin.math.abs
+import kotlin.math.min
 
 fun main() {
+    println("Part A: ${runP13("inputs/input13a.txt")}")
+    println("Part B: ${runP13("inputs/input13b.txt")}")
+    println("Part C: ${runP13("inputs/input13c.txt")}")
+}
+
+fun runP13(filename: String): Int {
     val grid =
-        Utils.readAsGrid("inputs/input13a.txt", null) { it }.filterValues { it != ' ' && it != '#' }.toMutableMap()
-    val startCoord = Utils.findCoord('S', grid)
-    val endCoord = Utils.findCoord('E', grid)
+        Utils.readAsGrid(filename, null) { it }.filterValues { it != ' ' && it != '#' }.toMutableMap()
+    val startCoord = Utils.findCoord('E', grid)
     val relaxedDists = Utils.generalizedDijkstra(
         startCoord, grid.keys,
         neighbors = { coord -> coord.manhattanNeighbors intersect grid.keys },
-        edgeWeight = { c1, c2 -> abs(heightP13(grid[c1]!!) - heightP13(grid[c2]!!)) + 1 },
+        edgeWeight = { c1, c2 ->
+            val heightDiff = abs(heightP13(grid[c1]!!) - heightP13(grid[c2]!!))
+            min(heightDiff, 10 - heightDiff) + 1
+        }
     )
-    println("Part A: ${relaxedDists.first[endCoord]}")
+    val endCoords = grid.filterValues { it == 'S' }.keys
+    return endCoords.minOf { relaxedDists.first[it]!! }
 }
 
 fun heightP13(char: Char): Int {
